22f880d5355cfa9856532813b925743821ccbc75
wwlr9 <tiange.hou18@gmail.com>
2021-12-12 18:22:55 -0500

    TheUPS
    Thebodyeverywhereandhere

--- TheUPS/sketch.js
+++ TheUPS/sketch.js
@@ -46,11 +46,11 @@ function setup() {
   video.size(width,height);
   video.hide();
 
- 
-  
+
+
   p5lm = new p5LiveMedia(this,"DATA",null,"myRoom");
   p5lm.on('data',gotPartnerPose);
-  
+
   handpose = ml5.handpose(video, {flipHorizontal: true},modelReady);
 
 }
@@ -65,9 +65,9 @@ function gotPartnerPose(data,id){
 }
 
 function generateTau(){
-  
+
   generateDegrade()
-  
+
   tint(Tautint1,Tautint1);
   image(TAUp,width/3.5,height/50);
   tint(Tautint2,Tautint2);
@@ -78,7 +78,7 @@ function generateTau(){
   image(TAUp,width-100,height/3);
   tint(Tautint5,Tautint5);
   image(TAUp,width/70,height/20);
-  
+
   noStroke();
   fill(255,0,0,0);
   ellipse(width/3.5+50,height/50+50,70);
@@ -86,8 +86,8 @@ function generateTau(){
   ellipse(width/8+50,height-100+50,70);
   ellipse(width-100+50,height/3+50,70);
   ellipse(width/70+50,height/20+50,70);
-  
-  
+
+
 }
 
 function generateDegrade(){
@@ -102,7 +102,7 @@ function generateDegrade(){
   image(degrade,width-100,height/3);
   tint(degradetint5,degradetint5);
   image(degrade,width/70,height/20);
-  
+
 }
 
 function comicsBack() {
@@ -110,23 +110,23 @@ function comicsBack() {
   const currentPixels = video.pixels;
   for(let y=0;y<height;y++){
     for(let x=0;x<width;x++){
-      
+
       const i = (y*width+x)*4;
       const diffR = abs(currentPixels[i+0]-prevPixels[i+0]);
       const diffG = abs(currentPixels[i+1]-prevPixels[i+1]);
       const diffB = abs(currentPixels[i+2]-prevPixels[i+2]);
-      
+
       prevPixels[i+0] = currentPixels[i+0];
       prevPixels[i+1] = currentPixels[i+1];
       prevPixels[i+2] = currentPixels[i+2];
 
       const avgDiff = (diffR + diffG+ diffB)/3;
-      if (avgDiff < 100) { 
+      if (avgDiff < 100) {
         currentPixels[i+0] = 0;
         currentPixels[i+1] = 0;
         currentPixels[i+2] = 0;
-      } 
-      else { 
+      }
+      else {
         currentPixels[i+0] = 255;//293
         currentPixels[i+1] = 255;//73
         currentPixels[i+2] = 255;//129
@@ -134,14 +134,14 @@ function comicsBack() {
     }
   }
   video.updatePixels();
-  
+
   noTint();
   push();
   translate(width,0);
   scale(-1,1);
   image(video, 0,0);
   pop();
-  
+
 }
 
 function e3Move(){
@@ -149,47 +149,47 @@ function e3Move(){
    //me
   if (hands && hands.length > 0) {
     let hand = hands[0];
-    let landmarks = hand.landmarks;    
+    let landmarks = hand.landmarks;
     fill(293,73,129,0);
     noStroke();
     for (let i = 0; i < landmarks.length; i++) {
-      let [x, y, z] = landmarks[i];     
+      let [x, y, z] = landmarks[i];
       //my palm
       if(i==0){
         // tint(255, 127);
         noTint();
-        ellipse(x+20,y-70,50);    
-        image(E3,x-20,y-120);   
+        ellipse(x+20,y-70,50);
+        image(E3,x-20,y-120);
         var d1 = dist(x+20,y-70, width/3.5+50,height/50+50);
         if(d1<50+70){
-          Tautint1 = 0;  
+          Tautint1 = 0;
           console.log('ub');
-          
+
         }
         var d2 = dist(x+20,y-70, width-200+50,height/7+50);
         if(d2<50+70){
-          Tautint2 = 0;  
+          Tautint2 = 0;
            console.log('ub');
         }
         var d3 = dist(x+20,y-70, width/8+50,height-100+50);
         if(d3<50+70){
-          Tautint3 = 0;  
+          Tautint3 = 0;
            console.log('ub');
         }
         var d4 = dist(x+20,y-70, width-100+50,height/3+50);
         if(d4<50+70){
-          Tautint4 = 0;  
+          Tautint4 = 0;
           console.log('ub');
         }
         var d5 = dist(x+20,y-70, width/70+50,height/20+50);
         if(d5<50+70){
-          Tautint5 = 0;  
+          Tautint5 = 0;
           console.log('ub');
         }
-      }      
+      }
     }
   }
-  
+
   proteasomeMove();
 }
 
@@ -197,54 +197,54 @@ function proteasomeMove(){
   //partner
   if (partnerPose && partnerPose.length > 0) {
     let partnerhand = partnerPose[0];
-    let partnerlandmarks = partnerhand.landmarks;    
+    let partnerlandmarks = partnerhand.landmarks;
     fill(101,211,156,0);
     noStroke();
     for (let i = 0; i < partnerlandmarks.length; i++) {
-      let [x, y, z] = partnerlandmarks[i];     
-      
+      let [x, y, z] = partnerlandmarks[i];
+
       //partner palm
       if(i==0){
         noTint();
-        ellipse(x,y,10);    
-        image(proteasome,x,y);  
-        
-        
+        ellipse(x,y,10);
+        image(proteasome,x,y);
+
+
         var d6 = dist(x+40,y+50, width/3.5+50,height/50+50);
         if(d6<50+70 && Tautint1 == 0){
-          degradetint1 = 0;  
+          degradetint1 = 0;
           console.log('clear!');
         }
         var d7 = dist(x+40,y+50, width-200+50,height/7+50);
         if(d7<50+70 && Tautint2 == 0){
-          degradetint2 = 0;  
+          degradetint2 = 0;
           console.log('clear!');
         }
         var d8 = dist(x+40,y+50, width/8+50,height-100+50);
         if(d8<50+70 && Tautint3 == 0){
-          degradetint3 = 0;  
+          degradetint3 = 0;
           console.log('clear!');
         }
         var d9 = dist(x+40,y+50, width-100+50,height/3+50);
         if(d9<50+70 && Tautint4 == 0){
-          degradetint4 = 0;  
+          degradetint4 = 0;
           console.log('clear!');
         }
         var d10 = dist(x+40,y+50, width/70+50,height/20+50);
         if(d10<50+70 && Tautint5 == 0){
-          degradetint5 = 0;  
+          degradetint5 = 0;
           console.log('clear!');
         }
-      }      
+      }
     }
   }
-  
+
   if(degradetint1 == 0 && degradetint2 == 0 && degradetint3 == 0 && degradetint4 == 0 && degradetint5 == 0){
     console.log('end');
     //game end
-    document.getElementById("success").textContent = 'game end';
+    document.getElementById("success").textContent = 'Game end. You two collaborate successfully!';
   }
-  
+
 }
 
 function draw() {
@@ -254,5 +254,5 @@ function draw() {
     // comicsBack();
     e3Move();
    }
-  
+
 }
